a31d1609a4e4af6dcdfd6a7c25ce18914ba1c883
Exybore <exybore@becauseofprog.fr>
2017-05-24 20:52:57 +0200

    4 colors / mobile header / grid improvements

---
+++ javascript/nav.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+  var mobileBtn = document.querySelector("a.exy-header__mobile-btn");
+  mobileBtn.addEventListener("click", function () {
+    var mobileNav = document.querySelector("ul.exy-header__mobile-nav");
+    mobileNav.style.display = "flex";
+  });
+}
+
+function mobile_desactivate() {
+  var mobileNav = document.querySelector("ul.exy-header__mobile-nav");
+  mobileNav.style.display = "none";
+}
