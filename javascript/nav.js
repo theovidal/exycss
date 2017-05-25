cbf7bf82fee9d5e2d3f54c79f14e11430a9c1c01
Exybore <exybore@becauseofprog.fr>
2017-05-25 19:15:48 +0200

    Footer / More enlarge & spacers / All colors / Blockquotes / Vertical texts / Borders / More tools / Subtitles

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,12 +1,14 @@
 window.onload=function(){
-  var mobileBtn = document.querySelector("a.exy-header__mobile-btn");
+  var mobileBtn = document.querySelector("header.exy-header__container div.exy-header__mobile-menu a.exy-header__mobile-btn");
   mobileBtn.addEventListener("click", function () {
     var mobileNav = document.querySelector("ul.exy-header__mobile-nav");
-    mobileNav.style.display = "flex";
+    mobileNav.style.display = "block";
+    var mobileNavFlex = document.querySelector("ul.exy-header__mobile-nav.exy-header__oneline-mobile");
+    mobileNavFlex.style.display = "flex";
   });
-}
-
-function mobile_desactivate() {
-  var mobileNav = document.querySelector("ul.exy-header__mobile-nav");
-  mobileNav.style.display = "none";
+  var mobileClose = document.querySelector("ul.exy-header__mobile-nav a.exy-header__close-mobile");
+  mobileClose.addEventListener("click", function () {
+    var mobileNav = document.querySelector("ul.exy-header__mobile-nav");
+    mobileNav.style.display = "none";
+  });
 }
